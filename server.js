bdc29c774e67423b40f56b3eb6ea8a8281fc34a6
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-04-26 19:19:57 -0500

    created the queries for the view all options

--- server.js
+++ server.js
@@ -81,3 +81,51 @@ const promptUser = () => {
             };
         });
 };
+
+// view all departments
+showDepartments = () => {
+    console.log('Currently viewing all departments...');
+
+    const sql = `SELECT department.id AS id, department.name AS department FROM department`;
+
+    connection.promise().query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser();
+    });
+};
+
+// view all roles
+showRoles = () => {
+    console.log('Currently viewing all roles...');
+
+    const sql = `
+        SELECT role.id, role.title, department.name AS department FROM role 
+        INNER JOIN department ON role.department_id = department.id
+    `;
+
+    connection.promise().query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser();
+    });
+};
+
+// view all employees
+showEmployees = () => {
+    console.log('Currently viewing all employees...');
+
+    const sql = `
+        SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT (manager.first_name, " ", manager.last_name) AS manager
+        FROM employee
+        LEFT JOIN role ON employee.role_id = role.id
+        LEFT JOIN department ON role.department_id = department.id
+        LEFT JOIN employee manager ON employee.manager_id = manager.id
+    `;
+
+    connection.promise().query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser();
+    });
+};
